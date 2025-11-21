2692c5856a1af9bb6d3eb8abe192df625d322bc9
Achsaf Atzmon <achsaf6@gmail.com>
2025-11-21 13:11:30 +0200

    hydration fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
-import { MapView } from "./components/MapView/MapView";
+"use client";
+
+import dynamic from "next/dynamic";
 import { BattlemapProvider } from "./providers/BattlemapProvider";
 import { CharacterProvider } from "./providers/CharacterProvider";
+
+const MapView = dynamic(
+  () => import("./components/MapView/MapView").then((mod) => mod.MapView),
+  {
+    ssr: false,
+  }
+);
 
 export default function Home() {
   return (
